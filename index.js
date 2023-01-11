2bb9bf82863a11f2c49353964368d1701873ec01
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-01-11 12:04:08 +0100

    Completed First Iteration.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // Iteration 1: Names and Input
+console.log("I'm ready!");
 
+const hacker1 = "Deniz"
+console.log(`The driver's name is ${hacker1}.`)
+
+const hacker2 = "Max"
+console.log(`The navigator's name is ${hacker2}.`)
+
+const hacker3 = "Matan"
+console.log(`The navigator's name is ${hacker3}.`)
 
 // Iteration 2: Conditionals
 
